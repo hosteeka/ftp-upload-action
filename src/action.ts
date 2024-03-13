26a3a0befdb2b936ad7cab3fb9b2c2a1dc51c4c3
Melvin Otieno <o.melvinotieno@gmail.com>
2024-03-13 19:55:06 +0300

    Run the action

---
+++ src/action.ts
@@ -0,0 +1,37 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+export async function run(): Promise<void> {
+  try {
+    let base: string | undefined;
+    let head: string | undefined;
+
+    core.debug(`Payload keys: ${Object.keys(github.context.payload)}`);
+
+    // Check the event that triggered the action
+    const eventName = github.context.eventName;
+    switch (eventName) {
+      case 'push':
+        // Get the base and head of the Push event
+        base = github.context.payload.before;
+        head = github.context.payload.after;
+        break;
+      case 'pull_request':
+        base = github.context.payload.pull_request?.base.sha;
+        head = github.context.payload.pull_request?.head.sha;
+        break;
+      default:
+        core.setFailed(`This action does not support the event ${eventName}`);
+    }
+
+    if (!base || !head) {
+      core.setFailed('Could not get base and head');
+      return;
+    }
+
+    core.info(`Base commit: ${base}`);
+    core.info(`Head commit: ${head}`);
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
